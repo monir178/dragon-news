4adeeee28135c891d11f578f4f0a2cd652ef178c
Your Name <you@example.com>
2023-04-02 17:11:35 +0600

    vecel hosting link changed in routes

--- src/Pages/Shared/LeftSideNav/LeftSideNav.js
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.js
@@ -6,7 +6,7 @@ const LeftSideNav = () => {
     const [categories, setCategories] = useState([]);
 
     useEffect(() => {
-        fetch('http://localhost:5000/news-categories')
+        fetch('https://dragon-news-server-zeta-two.vercel.app/news-categories')
             .then(res => res.json())
             .then(data => setCategories(data))
     }, [])
